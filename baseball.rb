c649eac6beea97fa4926240e89d8e8b7caa04694
imdizi <jhkgus@naver.com>
2011-08-04 21:27:03 +0900

    전부 출력

--- baseball.rb
+++ baseball.rb
@@ -3,10 +3,11 @@ require 'mechanize'
 
 agent=Mechanize.new
 p=agent.get('http://kbodata.news.naver.com/m_rank/rank_team.asp')
-i = p.search('//table[@class="table_board2"]/tbody/tr')[0]
-arr = i.search('td')
-str = arr[0].inner_text
-str += " - " + arr[1].inner_text
-arr[-1].inner_text =~ /(\d+)승 (\d+)패 (\d+)무/
-str += " - " + $1 + " - " + $2 + " - " + $3
-puts str
+p.search('//table[@class="table_board2"]/tbody/tr').each do |i|
+  arr = i.search('td')
+  str = arr[0].inner_text
+  str += " - " + arr[1].inner_text
+  arr[-1].inner_text =~ /(\d+)승 (\d+)패 (\d+)무/
+  str += " - " + $1 + " - " + $2 + " - " + $3
+  puts str
+end
