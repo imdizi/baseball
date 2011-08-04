a8ef839710b05d50c69395feaa716297ee78940e
imdizi <jhkgus@naver.com>
2011-08-04 21:00:20 +0900

    필요한 정보만 뽑기

--- baseball.rb
+++ baseball.rb
@@ -3,5 +3,8 @@ require 'mechanize'
 
 agent=Mechanize.new
 p=agent.get('http://kbodata.news.naver.com/m_rank/rank_team.asp')
-puts p.search('//table[@class="table_board2"]/tbody/tr')[0].inner_text
-
+i = p.search('//table[@class="table_board2"]/tbody/tr')[0]
+arr = i.inner_text.split("\n")
+puts arr[0]
+puts arr[1]
+puts arr[-2]
