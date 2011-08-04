ed1e186329585e7ae8dcb217669475379f8cb3af
imdizi <jhkgus@naver.com>
2011-08-04 21:25:46 +0900

    하나 출력

--- baseball.rb
+++ baseball.rb
@@ -5,7 +5,8 @@ agent=Mechanize.new
 p=agent.get('http://kbodata.news.naver.com/m_rank/rank_team.asp')
 i = p.search('//table[@class="table_board2"]/tbody/tr')[0]
 arr = i.search('td')
-puts arr[0].inner_text
-puts arr[1].inner_text
-puts arr[-1].inner_text
-
+str = arr[0].inner_text
+str += " - " + arr[1].inner_text
+arr[-1].inner_text =~ /(\d+)승 (\d+)패 (\d+)무/
+str += " - " + $1 + " - " + $2 + " - " + $3
+puts str
